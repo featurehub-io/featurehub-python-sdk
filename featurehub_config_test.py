6a76f013add2f5ff74e571ce03edd7a43a697ff0
Irina Southwell <irinasouthwell@gmail.com>
2021-08-29 18:14:45 +1200

    add some config tests

--- featurehub_config_test.py
+++ featurehub_config_test.py
@@ -22,3 +22,10 @@ class FeatureHubConfigTest(unittest.TestCase):
         self.assertEqual(config._api_keys, ['default/29517115-da60-4b64-99db-9da017561edd/7NZq23UuFEdmpcwxx7lkKSH0QH1EIS*dmGCHaOErbXyMLfzHCe0',
                                             'default/29517115-da60-4b64-99db-9da017561edd/a2eK0IJEeXdBrgiv3sLKaaxEZDy6uJ55F85RhIdK*dmGCHaOErbXyMLfzHCe0'])
 
+    def test_no_edge_url_provided(self):
+        with self.assertRaises(TypeError):
+            FeatureHubConfig("", self.client_eval_key)
+
+    def test_no_key_provided(self):
+        with self.assertRaises(TypeError):
+            FeatureHubConfig("bla")
