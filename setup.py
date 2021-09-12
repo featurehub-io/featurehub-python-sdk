d6c0f4c8f722caf5bfc74531f7deeae6f7159922
Irina Southwell <irinasouthwell@gmail.com>
2021-09-12 17:36:09 +1200

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='featurehub-python-sdk',
+    version='1.0',
+    description='Official Python SDK for FeatureHub',
+    author='FeatureHub.io',
+    author_email='info@featurehub.io',
+    packages=['featurehub-python-sdk'],
+    install_requires=['httpx[http2]==0.19.*',
+                      'respx==0.17.*'],
+)
